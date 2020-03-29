041302aaf46e2362a0495e34da7c6692e47f6249
sovaz1997 <sovaz1997@yandex.ru>
2020-03-29 20:13:46 +0300

    fix: fix

---
+++ README.md
@@ -0,0 +1,12 @@
+# Caesar CLI
+
+
+## Installation
+
++ Clone repo
++ Select folder `cd caesar_cli/`
++ Install dependencies `npm install`
+
+## Run program
+
++ Make `node index.js <params>`
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const ALG_LEN = 26;
 
 const isUpperCase = (letter) => {
   return letter[0] >= 'A' && letter[0] <= 'Z';
-}
+};
 
 const isLowerCase = (letter) => {
   return letter[0] >= 'a' && letter[0] <= 'z';
-}
+};
 
 const symCode = (letter) => {
   if (isLowerCase(letter)) {
@@ -69,11 +69,10 @@ const CaesarsCode = (chunk, shift, encode) => {
   return newStr;
 };
 
-
 const ERROR_CODES = {
   BAD_ACTION: { code: 1, message: 'Invalid action!' },
-  BAD_INPUT: { code: 2, message: 'Invalid input!'},
-  BAD_OUTPUT: { code: 3, message: 'Invalid output!'},
+  BAD_INPUT: { code: 2, message: 'Invalid input!' },
+  BAD_OUTPUT: { code: 3, message: 'Invalid output!' },
 };
 
 const sendError = (errorType) => {
@@ -98,7 +97,9 @@ if (program.action !== ENCODE && program.action !== DECODE) {
 let inputStream;
 
 if (program.input) {
-  inputStream = fs.createReadStream(program.input).on('error', () => sendError('BAD_INPUT'));
+  inputStream = fs
+    .createReadStream(program.input)
+    .on('error', () => sendError('BAD_INPUT'));
 } else {
   inputStream = process.stdin;
 }
@@ -106,18 +107,21 @@ if (program.input) {
 let outputStream;
 
 if (program.output) {
-  outputStream = fs.createWriteStream(program.output).on('error', () => sendError('BAD_OUTPUT'));
+  outputStream = fs
+    .createWriteStream(program.output)
+    .on('error', () => sendError('BAD_OUTPUT'));
 } else {
   outputStream = process.stdout;
 }
 
-const encode = (program.action === ENCODE);
+const encode = program.action === ENCODE;
 
 inputStream
-  .pipe(through2(function (chunk, enc, callback) {
-    this.push(CaesarsCode(chunk, program.shift, encode));
-
-    callback();
-   }))
+  .pipe(
+    through2(function f(chunk, enc, callback) {
+      this.push(CaesarsCode(chunk, program.shift, encode));
+      callback();
+    })
+  )
   .pipe(outputStream)
-  .on('finish', () => console.warn('Success!'))
+  .on('finish', () => console.warn('Success!'));
